f58b4c0b287bc6cf64d3af60a0f421107730ef9f
mafe <mvalenzuela@unal.edu.co>
2025-06-22 18:31:01 -0500

    sol

--- homework/pregunta_05.py
+++ homework/pregunta_05.py
@@ -5,6 +5,17 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
+def read_dataset(filepath):
+    """
+    Reads a tab-separated CSV file and returns a list of rows,
+    where each row is a list of column values.
+    """
+    rows = []
+    with open(filepath, "r") as file:
+        for line in file:
+            cols = line.strip().split("\t")
+            rows.append(cols)
+    return rows
 
 def pregunta_05():
     """
@@ -15,23 +26,23 @@ def pregunta_05():
     [('A', 9, 2), ('B', 9, 1), ('C', 9, 0), ('D', 8, 3), ('E', 9, 1)]
 
     """
-
-with open('./files/input/data.csv', 'r') as file:
-        lines = file.readlines()
-
-    max_min_values = {}
-
-    for line in lines:
-        columns = line.split()
-        letter = columns[0]
-        value = int(columns[1])
-        if letter in max_min_values:
-            max_min_values[letter][0] = max(max_min_values[letter][0], value)
-            max_min_values[letter][1] = min(max_min_values[letter][1], value)
+    data = read_dataset("files/input/data.csv")
+    results = {}
+    for row in data:
+        letter = row[0]
+        value = int(row[1])
+        if letter not in results:
+            results[letter] = [value, value]
         else:
-            max_min_values[letter] = [value, value]
+            if value > results[letter][0]:
+                results[letter][0] = value
+            if value < results[letter][1]:
+                results[letter][1] = value
+    final_result = []
+    for letter in sorted(results.keys()):
+        maximum, minimum = results[letter]
+        final_result.append((letter, maximum, minimum))
+    return final_result
 
-    result = [(letter, values[0], values[1]) for letter, values in max_min_values.items()]
-    result.sort()
 
-    return result
+
